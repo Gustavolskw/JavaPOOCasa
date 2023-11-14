d625bc219b478d253c8e5a69748ac2da3300d6b0
Gustavolskw <gustavolschmidt13@gmail.com>
2023-11-14 20:53:22 -0300

    Java POO Casa Repository

---
+++ src/Control/Controller.java
@@ -0,0 +1,13 @@
+package Control;
+
+public class Controller {
+	  public void showMenu(){
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	  }
+}
---
+++ src/Control/Main.java
@@ -0,0 +1,7 @@
+package Control;
+
+public class Main {
+	 public static void main(String[] args) {
+		
+	 }
+}
